4323b466dbddd4ad1790387d8a1209cec80fbf9a
macina <macina@key-4.com>
2024-05-15 13:58:31 +0200

    add : errors, logs controller with httpstatus

--- src/main/java/com/key4/visualizr/controller/controller.java
+++ src/main/java/com/key4/visualizr/controller/controller.java
@@ -2,10 +2,11 @@ package com.key4.visualizr.controller;
 
 import com.key4.visualizr.model.entity.ErrorEntity;
 import com.key4.visualizr.model.entity.LogsEntity;
-import com.key4.visualizr.repository.LogsRepository;
 import com.key4.visualizr.service.impl.ErrorService;
 import com.key4.visualizr.service.impl.LogsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +24,31 @@ public class controller {
     LogsService logsService;
 
     @GetMapping("logs")
-    public List<LogsEntity> getAllLogs(){
-        return logsService.fetchAllLogs();
+    public ResponseEntity<List<LogsEntity>> getAllLogs(){
+        try {
+            List<LogsEntity> logsList = logsService.fetchAllLogs();
+
+            if(logsList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(logsList,HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @GetMapping("errors")
-    public List<ErrorEntity> getAllErrors(){
-        return errorService.fetchAllErrors();
-    }
+    public ResponseEntity<List<ErrorEntity>> getAllErrors(){
+        try{
+            List<ErrorEntity> errorsList = errorService.fetchAllErrors();
 
+            if(errorsList.isEmpty()){
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(errorsList, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ src/main/java/com/key4/visualizr/reader/CsvReader.java
@@ -0,0 +1,24 @@
+package com.key4.visualizr.reader;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+public class CsvReader {
+
+    private final String src = "";
+
+    CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
+
+    public void readCsv()  {
+
+
+    }
+}
+
+
