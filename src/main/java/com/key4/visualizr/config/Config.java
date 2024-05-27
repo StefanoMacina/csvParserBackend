ae8ca7b9fab81ce07e7c1828992999a89e428638
macina <macina@key-4.com>
2024-05-27 11:20:21 +0200

    add errors cors

--- src/main/java/com/key4/visualizr/config/Config.java
+++ src/main/java/com/key4/visualizr/config/Config.java
@@ -19,6 +19,13 @@ public class Config implements WebMvcConfigurer {
                         "Access-Control-Request-Headers", "Access-Control-Allow-Origin")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
                 .allowCredentials(true);
+        registry.addMapping("/api/v1/errorsUpload")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers", "Access-Control-Allow-Origin")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true);
         registry.addMapping("/api/v1/pagerrlogs")
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
